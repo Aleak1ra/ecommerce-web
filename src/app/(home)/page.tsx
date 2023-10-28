80d0cb5f96b81b01c1e81fabe1a41cd3bd812fe6
Aleak1ra <alexandre_bernardi95@hotmail.com>
2023-10-27 22:22:34 -0300

    feat: scroll product list deal component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,33 @@
-"use client";
 
 import Image from "next/image";
 import Categories from "./components/categories";
+import { prismaClient } from "@/lib/prisma";
+import ProductList from "./components/product-list";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
   return (
     <div className="p-5">
       <Image
-      src="/banner-home-01.png"
-      width={150}
-      height={350}
-      alt="Até 55% de desconto"
-      className="h-auto w-full"
-      sizes="100vw"
+        src="/banner-home-01.png"
+        width={150}
+        height={350}
+        alt="Até 55% de desconto"
+        className="h-auto w-full"
+        sizes="100vw"
       />
       <div className="mt-8">
-        
-      <Categories/>
+        <Categories />
+      </div>
+      <div>
+        <ProductList products={deals}/>
       </div>
     </div>
   );
