5b4c51eb3ff97df9c0db11ec4974828b1d2e79be
Aleak1ra <alexandre_bernardi95@hotmail.com>
2023-10-28 23:37:03 -0300

    feat: add discount products list in home page

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,12 +1,28 @@
 import { Product } from "@prisma/client";
+import Image from "next/image";
 
-interface ProductItemProps{
-    product: Product
+interface ProductItemProps {
+  product: Product;
 }
 
+const ProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex h-[170px] w-[156px] items-center justify-center rounded-lg border-accent">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          style={{
+            objectFit: "contain",
+          }}
+          alt={product.name}
+        />
+      </div>
+    </div>
+  );
+};
 
-const ProductItem = ({product}: ProductItemProps) => {
-    return ( <h1>{product.name}</h1> );
-}
- 
 export default ProductItem;
