d4b89d9708cc2602346c71e63785c37c43b6ea7b
Aleak1ra <alexandre_bernardi95@hotmail.com>
2023-10-31 09:47:22 -0300

    feat: add footer components

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className=" bg-accent px-8 py-4 text-[0.625rem]">
+      2023 Copyright <span className="font-semibold">ALEXANDRE.dev</span>{" "}
+    </div>
+  );
+};
+
+export default Footer;
