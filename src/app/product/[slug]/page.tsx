a47944d2e52d9d587f4a43b7bc9810610518ea98
Aleak1ra <alexandre_bernardi95@hotmail.com>
2023-12-22 10:33:09 -0300

    feat: add categories nav bar all pages

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import ProductInfo from "./components/product-info";
 import { computeProductTotalPrice } from "@/helpers/product";
 import ProductList from "@/components/ui/product-list";
 import SectionTitle from "@/components/ui/section-title";
+import Categories from "@/app/(home)/components/categories";
 
 interface ProductDetailsPageProps {
   params: {
@@ -36,7 +37,8 @@ const ProductDetailsPage = async ({
   if (!product) return null;
 
   return (
-    <div className="mx-auto flex flex-col gap-8 pb-8 lg:w-9/12">
+    <div className="mx-auto mt-[30px] flex flex-col gap-8 pb-8 lg:w-9/12">
+      <Categories />
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
 
       <ProductInfo product={computeProductTotalPrice(product)}></ProductInfo>
